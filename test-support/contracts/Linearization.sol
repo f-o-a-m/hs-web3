bff97c7d623fd648eeac3bdb383e08b918d9c8a1
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2018-07-24 17:48:42 -0400

    add some more events to Linearization.sol for variety

--- test-support/contracts/Linearization.sol
+++ test-support/contracts/Linearization.sol
@@ -1,9 +1,11 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.22;
 
 contract Linearization {
 
   event E1(address sender, uint amount);
   event E2(string tag, bytes4 uuid);
+  event E3(address txOrigin, uint blockNumber);
+  event E4(bytes4 sig, bytes32 blockHash);
 
   function e12() public {
     emit E1(msg.sender, 0);
@@ -21,5 +23,13 @@ contract Linearization {
 
   function e2() public {
     emit E2("hello", 0xdeadbeef);
+  }
+
+  function e3() public {
+    emit E3(tx.origin, block.number);
+  }
+
+  function e4() public {
+    emit E4(msg.sig, blockhash(block.number));
   }
 }
