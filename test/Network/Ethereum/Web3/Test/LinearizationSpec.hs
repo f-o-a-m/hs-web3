9add0f8d88b5ee35ae6ced129c9310ca020b69b8
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2018-07-24 17:54:22 -0400

    lin. flood spec, some flood helpers

--- test/Network/Ethereum/Web3/Test/LinearizationSpec.hs
+++ test/Network/Ethereum/Web3/Test/LinearizationSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE LambdaCase            #-}
@@ -7,8 +8,10 @@
 {-# LANGUAGE NamedFieldPuns        #-}
 {-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
 
 -- Module      :  Network.Ethereum.Web3.Test.SimpleStorage
 -- Copyright   :  Alexander Krupenkin 2016
@@ -24,12 +27,21 @@
 
 module Network.Ethereum.Web3.Test.LinearizationSpec where
 
-import           Control.Concurrent.Async         (wait, waitAny)
+import           Control.Concurrent               (forkIO)
+import           Control.Concurrent.Async         (Async, async, wait)
 import           Control.Concurrent.MVar
-import           Control.Monad                    (void)
-import           Control.Monad.IO.Class           (liftIO)
+import           Control.Concurrent.STM           (atomically)
+import           Control.Concurrent.STM.TQueue    (TQueue, flushTQueue,
+                                                   newTQueueIO, writeTQueue)
+import           Control.Concurrent.STM.TSem      (TSem, newTSem, signalTSem,
+                                                   waitTSem)
+import           Control.Monad                    (forM, void)
+import           Control.Monad.IO.Class           (MonadIO, liftIO)
+import           Control.Monad.Trans.Reader       (ReaderT, ask)
 import           Data.Default
 import           Data.Either
+import           Data.List                        (sort)
+import           Data.Maybe                       (fromJust)
 import           Network.Ethereum.Contract.Event
 import           Network.Ethereum.Contract.TH
 import           Network.Ethereum.Web3            hiding (convert)
@@ -37,12 +49,24 @@ import qualified Network.Ethereum.Web3.Eth        as Eth
 import           Network.Ethereum.Web3.Test.Utils
 import           Network.Ethereum.Web3.Types
 import           System.Environment               (getEnv)
+import           System.Random                    (randomRIO)
 import           Test.Hspec
 
 [abiFrom|test-support/build/contracts/abis/Linearization.json|]
 
 spec :: Spec
-spec = makeEnv `before` linearizationSpec
+spec = do
+  makeEnv `before` linearizationSpec
+  makeEnv `before` floodSpec
+
+floodCount :: Int
+floodCount = 200
+
+-- waitTSem will block until the counter is positive (i.e., > 0)
+-- so if there's -(floodCount - 1), that means when the floodCount `signalTSem`s are done
+-- there will be 1 unit left in the TSem for the waitTSem at the end of a test
+floodSemCount :: Int
+floodSemCount = -(floodCount - 1)
 
 linearizationSpec :: SpecWith (ContractsEnv, Address)
 linearizationSpec = describe "can bundle and linearize events" $ do
@@ -61,6 +85,53 @@ linearizationSpec = describe "can bundle and linearize events" $ do
       res <- takeMVar var
       res `shouldSatisfy` isRight
 
+singleFlood :: forall m. (MonadIO m) => Address -> Address -> m Hash
+singleFlood from to = liftIO $ do
+  rando :: Int <- randomRIO (1, 4)
+  let theCall = callFromTo from to
+      fnToCall = case rando of
+                   1 -> e1
+                   2 -> e2
+                   3 -> e3
+                   4 -> e4
+                   _ -> error "got a number outside of (1,4) after randomR (1,4)"
+
+  retryWeb3Configured (fnToCall theCall)
+
+floodSpec :: SpecWith (ContractsEnv, Address)
+floodSpec = describe "can correctly demonstrate the difference between `multiEvent` and multiple `event'`s" $ do
+      it "properly linearizes with `multiEvent` when flooded" $ \(ContractsEnv{linearization}, primaryAccount) -> do
+        recvSem <- liftIO . atomically $ newTSem floodSemCount
+        q <- monitorAllFourMulti linearization (Just recvSem)
+        sleepBlocks 5 -- to let the filter settle so we dont block indefinitely on missing events?
+
+        -- flood em and wait for all to finish
+        hashes <- forM [1..floodCount] . const $ singleFlood primaryAccount linearization
+        void $ forM hashes awaitTxMined
+
+        -- wait for all multiEvents to be received and flush em out
+        receivedEvents <- liftIO . atomically $ flushTQueue q
+
+        -- the events pushed into the TQueue should already be sorted if they happened in the right order
+        length receivedEvents `shouldSatisfy` (>= (floodSemCount `div` 4)) -- did we get at least 1/4 of the events?
+        sort receivedEvents `shouldBe` receivedEvents
+
+      it "doesn't properly linearize with multiple `event`s when flooded" $ \(ContractsEnv{linearization}, primaryAccount) -> do
+        recvSem <- liftIO . atomically $ newTSem floodSemCount
+        q <- monitorAllFourPar linearization (Just recvSem)
+        sleepBlocks 5 -- to let the filter settle so we dont block indefinitely on missing events?
+
+        -- flood em and wait for all to finish
+        hashes <- forM [1..floodCount] . const $ singleFlood primaryAccount linearization
+        void $ forM hashes awaitTxMined
+
+        -- wait for all multiEvents to be received and flush em out
+        receivedEvents <- liftIO . atomically $ flushTQueue q
+
+        -- the events pushed into the TQueue should not be sorted if they didnt come in in the right order
+        length receivedEvents `shouldSatisfy` (>= (floodSemCount `div` 4)) -- did we get at least 1/4 of the events?
+        sort receivedEvents `shouldNotBe` receivedEvents
+
 monitorE1OrE2
   :: Address
   -> IO (MVar (Either E1 E2))
@@ -68,7 +139,7 @@ monitorE1OrE2 addr = do
   var <- newEmptyMVar
   let fltr1 = (def :: Filter E1) { filterAddress = Just [addr] }
       fltr2 = (def :: Filter E2) { filterAddress = Just [addr] }
-      filters = fltr1 :? (fltr2 :? NilFilters)
+      filters = fltr1 :? fltr2 :? NilFilters
       handler1 e1 = do
         liftIO $ putMVar var (Left e1)
         pure TerminateEvent
@@ -78,3 +149,59 @@ monitorE1OrE2 addr = do
       handlers = H handler1 :& H handler2 :& RNil
   _ <- runWeb3Configured' $ multiEvent filters handlers
   pure var
+
+data EventTag = ETE1 | ETE2 | ETE3 | ETE4
+                deriving (Eq, Read, Show)
+
+instance {-# OVERLAPPING #-} Ord (EventTag, Integer, Integer) where
+  (_, b1, t1) `compare` (_, b2, t2) =
+    let bCmp = b1 `compare` b2
+     in if bCmp == EQ
+          then t1 `compare` t2
+          else bCmp
+
+monitorAllFourMulti
+  :: Address
+  -> Maybe TSem
+  -> IO (TQueue (EventTag, Integer, Integer)) -- (EventTag, BlockNumber, LogIndex)
+monitorAllFourMulti addr sem = do
+  q <- newTQueueIO
+  let f :: forall a. Default (Filter a) => Filter a
+      f = defFilter addr
+      h = enqueueingHandler sem q
+      filters = f @E1 :? f @E2 :? f @E3 :? f @E4 :? NilFilters
+      handlers = h ETE1 :& h ETE2 :& h ETE3 :& h ETE4 :& RNil
+  void . runWeb3Configured' $ multiEvent filters handlers
+  return q
+
+monitorAllFourPar
+  :: Address
+  -> Maybe TSem
+  -> IO (TQueue (EventTag, Integer, Integer)) -- (EventTag, BlockNumber, LogIndex)
+monitorAllFourPar addr sem = do
+  q <- newTQueueIO
+  let f :: forall a. Default (Filter a) => Filter a
+      f = defFilter addr
+      h = enqueueingHandler sem q
+      unH (H h) = h
+
+  void . runWeb3Configured' $ event' (f @E1) (unH $ h ETE1)
+  void . runWeb3Configured' $ event' (f @E2) (unH $ h ETE2)
+  void . runWeb3Configured' $ event' (f @E3) (unH $ h ETE3)
+  void . runWeb3Configured' $ event' (f @E4) (unH $ h ETE4)
+  return q
+
+defFilter :: forall a. Default (Filter a) => Address -> Filter a
+defFilter addr = (def :: Filter a) { filterAddress = Just [addr] }
+
+enqueueingHandler :: forall a. Maybe TSem -> TQueue (EventTag, Integer, Integer) -> EventTag -> Handler (ReaderT Change Web3 EventAction) a
+enqueueingHandler sem q tag = H . const $ do
+  Change{..} <- ask
+  let bn = unQuantity $ fromJust changeBlockNumber
+      li = unQuantity $ fromJust changeLogIndex
+  liftIO . atomically $ do
+    writeTQueue q (tag, bn, li)
+    case sem of
+      Nothing      -> pure ()
+      Just recvSem -> signalTSem recvSem
+  pure ContinueEvent
--- test/Network/Ethereum/Web3/Test/Utils.hs
+++ test/Network/Ethereum/Web3/Test/Utils.hs
@@ -1,22 +1,28 @@
-{-# LANGUAGE DeriveGeneric     #-}
-{-# LANGUAGE LambdaCase        #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Network.Ethereum.Web3.Test.Utils
   ( makeEnv
   , ContractsEnv(..)
   , runWeb3Configured
   , runWeb3Configured'
+  , retryWeb3Configured
   , withAccounts
   , withPrimaryEthereumAccount
   , callFromTo
   , sleepSeconds
   , microtime
   , awaitBlock
+  , awaitTxMined
+  , sleepBlocks
   ) where
 
 import           Control.Concurrent                (MVar, threadDelay,
                                                     tryTakeMVar)
+import           Control.Exception                 (SomeException, catch,
+                                                    handle, throwIO)
 import           Control.Lens                      ((^?))
 import           Control.Monad.IO.Class            (liftIO)
 import           Data.Aeson                        (FromJSON, eitherDecode)
@@ -26,7 +32,7 @@ import qualified Data.ByteString.Lazy              as BSL
 import           Data.Default
 import           Data.Either                       (isRight)
 import           Data.List.Split                   (splitOn)
-import           Data.Maybe                        (fromMaybe)
+import           Data.Maybe                        (fromMaybe, isJust)
 import           Data.Ratio                        (numerator)
 import           Data.String                       (IsString, fromString)
 import qualified Data.Text                         as T
@@ -34,15 +40,36 @@ import           Data.Time.Clock.POSIX             (getPOSIXTime)
 import           Data.Traversable                  (for)
 import           GHC.Generics                      (Generic)
 import           Network.Ethereum.ABI.Prim.Address (Address)
-import           Network.Ethereum.Web3.Eth         (accounts, blockNumber)
+import           Network.Ethereum.Web3.Eth         (accounts, blockNumber,
+                                                    getTransactionReceipt)
 import           Network.Ethereum.Web3.Net         as Net
 import           Network.Ethereum.Web3.Provider    (JsonRpcProvider (..),
                                                     Provider (..), Web3,
-                                                    Web3Error, runWeb3')
-import           Network.Ethereum.Web3.Types       (Call (..), Quantity)
+                                                    Web3Error, runWeb3With)
+import           Network.Ethereum.Web3.Types       (Call (..), Hash, Quantity,
+                                                    TxReceipt (..))
+import           Network.HTTP.Client               (Manager, ManagerSettings,
+                                                    defaultManagerSettings,
+                                                    managerConnCount,
+                                                    managerRawConnection,
+                                                    managerRetryableException,
+                                                    newManager)
 import           System.Environment                (lookupEnv, setEnv)
+import           System.IO.Unsafe                  (unsafePerformIO)
 import           Test.Hspec.Expectations           (shouldSatisfy)
 
+-- shared manager used throughout the helpers here to prevent hammering geth from ruining everything
+-- this also retrys on ALL exceptions, including ConnectionResetByPeer and stuff like that
+sharedManager :: Manager
+sharedManager = unsafePerformIO $ newManager defaultManagerSettings
+    { managerConnCount = 5
+    , managerRetryableException = const False
+    , managerRawConnection = fixRawConnection retryOpenConnection
+    }
+
+    where retryOpenConnection = threadDelay 500000 >> managerRawConnection defaultManagerSettings
+          fixRawConnection f = f `catch` (\(_ :: SomeException) -> fixRawConnection f)
+{-# NOINLINE sharedManager #-}
 
 makeEnv :: IO (ContractsEnv, Address)
 makeEnv = do
@@ -75,15 +102,23 @@ makeContractsEnv = do
 runWeb3Configured :: Show a => Web3 a -> IO a
 runWeb3Configured f = do
     provider <- (flip Provider Nothing . HttpProvider) <$> rpcUri
-    v <- runWeb3' provider f
+    v <- runWeb3With sharedManager provider f
     v `shouldSatisfy` isRight
     let Right a = v in return a
 
 runWeb3Configured' :: Web3 a -> IO a
 runWeb3Configured' f = do
     provider <- (flip Provider Nothing . HttpProvider) <$> rpcUri
-    Right v <- runWeb3' provider f
+    Right v <- runWeb3With sharedManager provider f
     return v
+
+retryWeb3Configured :: Web3 a -> IO a
+retryWeb3Configured f = do
+    provider <- (flip Provider Nothing . HttpProvider) <$> rpcUri
+    v <- runWeb3With sharedManager provider f
+    case v of
+        Left _  -> threadDelay 1000000 >> retryWeb3Configured f
+        Right v -> return v
 
 withAccounts :: ([Address] -> IO a) -> IO a
 withAccounts f = runWeb3Configured accounts >>= f
@@ -106,8 +141,22 @@ microtime = numerator . toRational . (* 1000000) <$> getPOSIXTime
 
 awaitBlock :: Quantity -> IO ()
 awaitBlock bn = do
-    bn' <- runWeb3Configured blockNumber
+    bn' <- retryWeb3Configured blockNumber
     putStrLn $ "awaiting block " ++ show bn ++ ", currently " ++ show bn'
     if bn' >= bn
         then return ()
         else threadDelay 1000000 >> awaitBlock bn
+
+sleepBlocks :: Int -> IO ()
+sleepBlocks n = do
+    now <- retryWeb3Configured blockNumber
+    awaitBlock $ now + (fromIntegral n)
+
+awaitTxMined :: Hash -> IO TxReceipt
+awaitTxMined hash = do
+    mReceipt <- retryWeb3Configured $ getTransactionReceipt hash
+    case mReceipt of
+        Nothing -> threadDelay 1000000 >> awaitTxMined hash
+        Just receipt -> if isJust (receiptBlockHash receipt)
+          then return receipt
+          else threadDelay 1000000 >> awaitTxMined hash
